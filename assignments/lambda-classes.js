7f955b459f58758e020f701a9ee43c9ddc0abd6b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-24 15:57:07 -0500

    Created base class 'Person' and new instantiation of 'Person' base class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,17 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location:
+        this.gender = props.gender;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+const dude = new Person({
+    
+})
